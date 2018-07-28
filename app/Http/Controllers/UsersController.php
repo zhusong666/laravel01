61379f3946ba3d386bd6e79b4ae5bb69a6dffdd0
zhusong666 <851119622@qq.com>
2018-07-28 21:08:56 +0800

    用户注册完成

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
      public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+     public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = $request->except('_token','password_confirmation');
+
+        $user = User::create($user);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show',['user'=>$user]);
     }
 }
